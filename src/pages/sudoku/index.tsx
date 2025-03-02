b6b6cdea4c40720ac05f639552b3673684875950
yoonjinwoo <yjw3647@naver.com>
2025-03-02 12:49:37 +0900

    build 오류 3

--- src/pages/sudoku/index.tsx
+++ src/pages/sudoku/index.tsx
@@ -1,18 +1,6 @@
 import Layout from "@widgets/Layout";
 import SudokuGame from "./game";
 
-const initialBoard = [
-  [5, 3, null, null, 7, null, null, null, null],
-  [6, null, null, 1, 9, 5, null, null, null],
-  [null, 9, 8, null, null, null, null, 6, null],
-  [8, null, null, null, 6, null, null, null, 3],
-  [4, null, null, 8, null, 3, null, null, 1],
-  [7, null, null, null, 2, null, null, null, 6],
-  [null, 6, null, null, null, null, 2, 8, null],
-  [null, null, null, 4, 1, 9, null, null, 5],
-  [null, null, null, null, 8, null, null, 7, 9],
-];
-
 const Sudoku = () => {
   return (
     <Layout title="StrRegExp">
