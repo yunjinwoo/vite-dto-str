32add0dc54b4226c8a35adbddd8e05d6a4d9600f
yoonjinwoo <yjw3647@naver.com>
2025-05-04 17:58:57 +0900

    레이아웃추가

--- src/pages/word/index.tsx
+++ src/pages/word/index.tsx
@@ -1,17 +1,20 @@
-import { Routes, Route } from 'react-router-dom';
-import Settings from './settings';
-import Help from './help';
-import WordInfoGame from './WordInfoGame';
-import WordNewGame from './WordNewGame';
+import { Routes, Route } from "react-router-dom";
+import Settings from "./settings";
+import Help from "./help";
+import WordInfoGame from "./WordInfoGame";
+import WordNewGame from "./WordNewGame";
+import Layout from "@widgets/Layout";
 
 const WordGameRoutes = () => {
   return (
-    <Routes>
-      <Route index element={<WordNewGame />} />
-      <Route path="info" element={<WordInfoGame />} />
-      <Route path="settings" element={<Settings />} />
-      <Route path="help" element={<Help />} />
-    </Routes>
+    <Layout title="Word">
+      <Routes>
+        <Route index element={<WordNewGame />} />
+        <Route path="info" element={<WordInfoGame />} />
+        <Route path="settings" element={<Settings />} />
+        <Route path="help" element={<Help />} />
+      </Routes>
+    </Layout>
   );
 };
 
